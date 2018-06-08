a42f92741e8b4bc8fa0e948231b578e189353f2a
Akshay Cj <akshaycj999@gmail.com>
2018-06-09 04:13:46 +0530

    search feature

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-
+import Feed from '../Components/Feed' 
 import { Button, Welcome } from '@storybook/react/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
@@ -11,3 +11,8 @@ storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo(
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+
+  storiesOf('Feed', module)
+  .add('default', () => <Feed />)
+  .add('with title', () => <Feed title="hello" />)
